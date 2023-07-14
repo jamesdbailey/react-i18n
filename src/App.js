5a9eb74f40dd29ae81f9209652e38a9e5a20766c
James Bailey <git@jdb.name>
2023-07-14 13:36:57 -0400

    first localization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home';
+import Page2 from './Page2';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route path='/page2' element={<Page2 />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
